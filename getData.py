6a235c68032b02b081518db606e449716f778970
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2023-02-08 18:33:58 +0100

    Added getData methot

--- getData.py
+++ getData.py
@@ -1,3 +1,16 @@
+import json
+
+
 class getData:
-    def __init__():
-        pass
+    def __init__(self, path: str) -> None:
+        with open(path, 'rb') as f:
+            raw_data = f.read().decode('UTF-8').replace('\n', '')
+            self.data = json.loads(raw_data)
+
+    def get(self, key: str):
+        return self.data[key]
+
+
+if __name__ == '__main__':
+    data = getData("log1.txt")
+    print(data.get("tick_speed"))
