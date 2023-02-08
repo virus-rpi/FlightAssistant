a3512f95796b0086184fffcb81d92424415b2f77
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2023-02-08 22:40:46 +0100

    added drag n drop file

--- utility.py
+++ utility.py
@@ -2,15 +2,18 @@ import json
 
 
 class getData:
-    def __init__(self, path: str) -> None:
-        with open(path, 'rb') as f:
-            raw_data = f.read().decode('UTF-8').replace('\n', '')
-            self.data = json.loads(raw_data)
+    def __init__(self, path: str, dictionary) -> None:
+        if dictionary is None:
+            with open(path, 'rb') as f:
+                raw_data = f.read().decode('UTF-8').replace('\n', '')
+        else:
+            raw_data = dictionary
+        self.data = json.loads(raw_data)
 
     def get(self, key: str):
         return self.data[key]
 
 
 if __name__ == '__main__':
-    data = getData("log1.txt")
+    data = getData("log.txt", None)
     print("Test 1: Pass" if data.get("tick_speed") == 20 else "Test 2: Fail")
--- web_app.py
+++ web_app.py
@@ -2,10 +2,12 @@ from dash import Dash, html, dcc
 import plotly.express as px
 from utility import getData
 from pandas import DataFrame
+import base64
+from dash.dependencies import Input, Output, State
 
 app = Dash(__name__)
 
-data = getData("log1.txt")
+data = getData("log.txt", None)
 tick_data = data.get("flight_data")
 
 
@@ -41,7 +43,7 @@ def height_plot():
             y0=min(heights),
             y1=max(heights),
             fillcolor="green" if min(deployed_ticks) > 10 else "red",
-            opacity=0.9,
+            opacity=0.3,
         )
     if len(ground_ticks) > 0:
         fig.add_shape(
@@ -51,7 +53,7 @@ def height_plot():
             y0=min(heights),
             y1=max(heights) / 4,
             fillcolor="gray",
-            opacity=0.9,
+            opacity=0.3,
         )
     if len(ascent_ticks) > 0:
         fig.add_shape(
@@ -61,7 +63,7 @@ def height_plot():
             y0=min(heights),
             y1=max(heights) / 4,
             fillcolor="blue",
-            opacity=0.9,
+            opacity=0.3,
         )
     if len(descent_ticks) > 0:
         fig.add_shape(
@@ -70,8 +72,8 @@ def height_plot():
             x1=max(descent_ticks),
             y0=min(heights),
             y1=max(heights) / 4,
-            fillcolor="pink",
-            opacity=0.9,
+            fillcolor="yellow",
+            opacity=0.3,
         )
     fig.update_layout(template="plotly_dark")
     return fig
@@ -141,7 +143,7 @@ def avg_tick_speed():
     for i in tick_data:
         tick_time.append(i["tt"])
 
-    avg_tick_time = (sum(tick_time)/len(tick_time))
+    avg_tick_time = (sum(tick_time) / len(tick_time))
 
     return str(round(avg_tick_time, 2))
 
@@ -149,31 +151,55 @@ def avg_tick_speed():
 app.layout = html.Div(children=[
     html.H1(children='Fight Control', style={'color': 'white'}),
     html.H2(children='General information', style={'color': 'white'}),
-    html.P(children=f'Tick speed [ms]: {str(data.get("tick_speed"))} | Avg. tick calc speed [ms]: {avg_tick_speed()}'),
-    html.P(children=f'Open angle [°]: {data.get("open_angle")} | Close angle [°]: {data.get("close_angle")}'),
-    html.P(children=f'Start height [m]: {data.get("start_height")} | Deploy height [m]: {data.get("deploy_height")}'),
-    html.P(children=f'Wait time [s]: {data.get("wait_time")} | {"Timer activated" if data.get("timer_state") else "Timer deactivated"}'),
-    html.P(children=f'G threshold [g]: {data.get("g_threshold")}'),
+    html.P(children=f'Tick speed [ms]: {str(data.get("tick_speed"))} | Avg. tick calc speed [ms]: {avg_tick_speed()}',
+           id="tick"),
+    html.P(children=f'Open angle [°]: {data.get("open_angle")} | Close angle [°]: {data.get("close_angle")}',
+           id="angle"),
+    html.P(children=f'Start height [m]: {data.get("start_height")} | Deploy height [m]: {data.get("deploy_height")}',
+           id="height"),
+    html.P(
+        children=f'Wait time [s]: {data.get("wait_time")} | {"Timer activated" if data.get("timer_state") else "Timer deactivated"}',
+        id="time"),
+    html.P(children=f'G threshold [g]: {data.get("g_threshold")}', id="top_g"),
     html.H2(children='Height', style={'color': 'white'}),
     dcc.Graph(
-        id='graph',
+        id='height_graph',
         figure=height_plot()
     ),
     html.H2(children='Current', style={'color': 'white'}),
     dcc.Graph(
-        id='graph',
+        id='current_graph',
         figure=current_plot()
     ),
     html.H2(children='Rotation', style={'color': 'white'}),
     dcc.Graph(
-        id='graph',
+        id='rot_graph',
         figure=degrees_plot()
     ),
     html.H2(children='Acceleration', style={'color': 'white'}),
     dcc.Graph(
-        id='graph',
+        id='acceleration_graph',
         figure=acceleration_plot()
-    )
+    ),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=False
+    ),
+    html.P(id='output-data-upload'),
 ],
     style={
         'backgroundColor': '#111111',
@@ -182,6 +208,82 @@ app.layout = html.Div(children=[
         'color': 'white',
     }
 )
+
+
+def parse_contents(contents, filename):
+    global data, tick_data
+    content_string = contents.split(',')
+
+    content = str(base64.b64decode(content_string[1].encode('utf-8')).decode('utf-8'))
+
+    try:
+        data = getData("", content)
+        tick_data = data.get("flight_data")
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.P(filename),
+    ])
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename')
+              )
+def update_output(list_of_contents, list_of_names):
+    if list_of_contents is not None:
+        children = [parse_contents(list_of_contents, list_of_names)]
+        return children
+
+
+@app.callback(Output('tick', 'children'), Input('upload-data', 'contents'), )
+def update(_):
+    return f'Tick speed [ms]: {str(data.get("tick_speed"))} | Avg. tick calc speed [ms]: {avg_tick_speed()}'
+
+
+@app.callback(Output('angle', 'children'), Input('upload-data', 'contents'))
+def update(_):
+    return f'Open angle [°]: {data.get("open_angle")} | Close angle [°]: {data.get("close_angle")}'
+
+
+@app.callback(Output('height', 'children'), Input('upload-data', 'contents'))
+def update(_):
+    return f'Start height [m]: {data.get("start_height")} | Deploy height [m]: {data.get("deploy_height")}'
+
+
+@app.callback(Output('time', 'children'), Input('upload-data', 'contents'))
+def update(_):
+    return f'Wait time [s]: {data.get("wait_time")} | {"Timer activated" if data.get("timer_state") else "Timer deactivated"}'
+
+
+@app.callback(Output('top_g', 'children'), Input('upload-data', 'contents'))
+def update(_):
+    return f'G threshold [g]: {data.get("g_threshold")}'
+
+
+@app.callback(Output('height_graph', 'figure'), Input('upload-data', 'contents'))
+def update(_):
+    return height_plot()
+
+
+@app.callback(Output('current_graph', 'figure'), Input('upload-data', 'contents'))
+def update(_):
+    return current_plot()
+
+
+@app.callback(Output('rot_graph', 'figure'), Input('upload-data', 'contents'))
+def update(_):
+    return degrees_plot()
+
+
+@app.callback(Output('acceleration_graph', 'figure'), Input('upload-data', 'contents'))
+def update(_):
+    return acceleration_plot()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
